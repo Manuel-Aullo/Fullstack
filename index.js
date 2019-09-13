b2bf37d3a0547e5f1063574ebb1d01867a77f859
ManuelAP <manuel.aullo@cybercom.com>
2019-09-13 07:29:58 +0300

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const app = express(); // running express app
 
 app.get("/", (req, res) =>{
-    res.send({hi: "there"});
+    res.send({hi: "mate"});
 });
 
 const PORT = process.env.PORT || 5000;
