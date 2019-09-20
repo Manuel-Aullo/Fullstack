c3df89942981415c31fd5aa215e67fef533b38ec
ManuelAP <manuel.aullo@cybercom.com>
2019-09-20 13:25:52 +0300

    fake ./dev to prod

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: "asdasdntent.com",
+    googleClientSecret: "1h93Mzadsdad",
+    linkedinClientID: "77bkyadsc",
+    linkedinClientSecret: "ConP3CCsaj8",
+    mongoURI: "mosa:/saas978.@ds213538.mlasevelop",
+    cookieKey: "lkdkslbnlvsbdopv9sa9d80v"
+}
