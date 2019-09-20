f78ac70b3a8c5856938e92ec7cdb9330d49964c6
ManuelAP <manuel.aullo@cybercom.com>
2019-09-20 10:51:36 +0300

    auth in dev and prod

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,23 @@ module.exports = (app) => {
     app.get(
         '/auth/google/callback',
         passport.authenticate('google'));
+
+    app.get(
+        '/api/current_user', (req, res) => {
+            res.send(req.user);
+        });
+    
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+    
+    app.get(
+        '/auth/linkedin',
+        passport.authenticate('linkedin', {
+            scope: ['r_liteprofile', 'r_emailaddress']
+        }));
+
+    app.get('/auth/linkedin/callback',
+        passport.authenticate('linkedin'));
 }
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,21 @@
 const passport = require ("passport");
-const GoogleStrategy = require ("passport-google-oauth20");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const LinkedinStrategy = require("passport-linkedin-oauth2").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user =>{
+            done(null, user)
+        });
+});
 
 passport.use(
     new GoogleStrategy(
@@ -9,9 +24,35 @@ passport.use(
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback'
         }, (accessToken, refreshToken, profile, done) => {
-            console.log("accessToken", accessToken);
-            console.log("refreshToken", refreshToken);
-            console.log("profile", profile);
+            User.findOne({googleId: profile.id})// Asynchronous query to find the first record inside the collection with the id = profile.id
+                .then ((existingUser)=>{
+                    if(existingUser){
+                        // we already have a record with the given profile ID
+                        done(null, existingUser); //
+                    } else {
+                        // we don't have a user record with this id, create a new record
+                        new User({ googleId: profile.id})
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                }) 
+            
         }
     )
 );
+
+passport.use(
+    new LinkedinStrategy(
+        { 
+            clientID: keys.linkedinClientID,
+            clientSecret: keys.linkedinClientSecret,
+            callbackURL: '/auth/linkedin/callback'
+        }, (accessToken, refreshToken, profile, done) => {
+            console.log("accessToken:", accessToken);
+            console.log("refreshToken:", refreshToken);
+            console.log("profile:", profile);
+        }
+    )
+);
+
+
