12782da4be50a0f19bb0da39e01b8caff0733312
ManuelAP <manuel.aullo@cybercom.com>
2019-10-02 13:57:02 +0300

    starting with the frontend and redux

---
+++ client/src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import {FETCH_USER} from "./types";
+
+
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/current_user');
+    dispatch({ type: FETCH_USER, payload: res.data });
+  };
+// export const fetchUser = () => { // action creator with redux-thunk
+//     return function(dispatch) {
+//         axios
+//             .get("/api/current_user") // request to backend API and communicates to the auth reducer
+//             .then( res => dispatch({ type: FETCH_USER, payload: res }))
+//     }  
+// };
---
+++ client/src/components/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import { connect } from "react-redux"; // compatibility between react and redux libraries
+import * as actions from "../actions"; // import action creators
+
+import Header from "./Header";
+
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+class App extends Component {
+    componentDidMount() {
+        //call action creator to fetch the actual user or if it's logged in
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div>
+                 <BrowserRouter>
+                    <div>
+                        <Header/>
+                        <div className="container">
+                            <Route exact path="/" component={Landing}/>
+                            <Route exact path="/surveys" component={Dashboard}/>
+                            <Route path="/surveys/new" component={SurveyNew} />
+                        </div>
+                    </div>
+                 </BrowserRouter>
+            </div>
+        );
+    }
+    
+}
+
+export default connect (null, actions) (App);
